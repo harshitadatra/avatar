7a1d93d33bbacdc41e1f765099d8f7edc68688e3
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2021-09-17 11:13:52 +0530

    wire fetch call

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var btnTranslate=document.querySelector("#button-id");
 var txtInput =document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output")
-console.log(btnTranslate);
-console.log(txtInput);
-function clickHandler()
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+function getTranslationURL(text)
 {
-    outputDiv.innerText = "hajahaoige"+txtInput.value;
+    return serverURL + "?" + "text=" + text;
 }
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        }
 btnTranslate.addEventListener("click",
 clickHandler);
