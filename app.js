c30fc139d4d4fe165cf92f81d1503e94bdbeba5a
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2021-09-17 11:27:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var btnTranslate=document.querySelector("#button-id");
+var btnTranslate=document.querySelector("#btn-translate");
 var txtInput =document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output")
 var serverURL = "https://api.funtranslations.com/translate/navi.json"
